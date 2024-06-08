57a97a481b1a1ba17a2e83de81a18defb5033a7b
Mugyuu <7702653+hata6502@users.noreply.github.com>
2024-06-08 13:52:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-controlled-translation
+Use the browser's built-in translation feature in React for free and safely. 
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState, useSyncExternalStore } from "react";
+
+export const useLanguage = () =>
+  useSyncExternalStore(subscribeLanguage, getLanguageSnapshot);
+
+export const useTranslation = (text: string) => {
+  const [translatedText, setTranslatedText] = useState(text);
+
+  useEffect(() => {
+    const hiddenElement = document.createElement("span");
+    hiddenElement.innerText = text;
+    containerElement.append(hiddenElement);
+
+    const handleTranslation = () => {
+      translationObserver.disconnect();
+      try {
+        setTranslatedText(hiddenElement.innerText);
+      } finally {
+        translationObserver.observe(hiddenElement, {
+          subtree: true,
+          childList: true,
+          characterData: true,
+        });
+      }
+    };
+    const translationObserver = new MutationObserver(handleTranslation);
+    handleTranslation();
+
+    return () => {
+      translationObserver.disconnect();
+      hiddenElement.remove();
+    };
+  }, [text]);
+
+  return translatedText;
+};
+
+const subscribeLanguage = (callback: () => void) => {
+  const languageObserver = new MutationObserver(callback);
+  languageObserver.observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ["lang"],
+  });
+
+  return () => {
+    languageObserver.disconnect();
+  };
+};
+const getLanguageSnapshot = () => document.documentElement.lang;
+
+const containerElement = document.createElement("div");
+containerElement.classList.add("react-controlled-translation");
+containerElement.style.display = "none";
+document.body.append(containerElement);
